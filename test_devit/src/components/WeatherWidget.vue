cead3778d7c835d9402d4437e9ce35648143f38c
Vika <beletskaya.vika@gmail.com>
2022-07-22 17:19:40 +0300

    add pagination

--- test_devit/src/components/WeatherWidget.vue
+++ test_devit/src/components/WeatherWidget.vue
@@ -9,11 +9,27 @@
     <h2>Cities</h2>
     <div v-if="getWeatherData">
       <card-weather
-        v-for="(item, index) in getWeatherData"
+        v-for="(item, index) in getItems"
         :data="item"
         :key="item.id"
         @delete-card="deleteCard(index)"
       />
+      <vuejs-paginate
+          :page-count="getPaginateCount"
+          :prev-text="'<'"
+          :next-text="'>'"
+          :click-handler="paginateClickCallback"
+          :container-class="'pagination justify-content-center'"
+          :page-class="'page-item'"
+          :page-link-class="'page-link'"
+          :prev-class="'page-item'"
+          :prev-link-class="'page-link'"
+          :next-class="'page-item'"
+          :next-link-class="'page-link'"
+          :first-last-button="true"
+          :first-button-text="'<<'"
+          :last-button-text="'>>'"
+      ></vuejs-paginate>
     </div>
   </div>
 </template>
@@ -21,14 +37,17 @@
 <script>
 import LocationWeather from "@/components/LocationWeather";
 import CardWeather from "@/components/CardWeather";
+import VuejsPaginate from "vuejs-paginate";
 import { mapActions } from 'vuex'
 
 export default {
-  components: {LocationWeather, CardWeather},
+  components: {LocationWeather, CardWeather, VuejsPaginate},
   data() {
     return {
       incorrectCity: false,
-      noreload: true
+      noreload: true,
+      currentPage: 1,
+      perPage: 5,
     }
   },
   mounted() {
@@ -85,19 +104,55 @@ export default {
       let storage = JSON.parse(localStorage.getItem('data-weather'))
       storage.splice(index, 1)
       localStorage.setItem('data-weather', JSON.stringify(storage))
+    },
+    paginateClickCallback (pageNum) {
+      this.currentPage = Number(pageNum);
     }
   },
   computed: {
     getWeatherData () {
       return this.$store.state.weatherData
+    },
+    getItems() {
+      let start = (this.currentPage - 1) * this.perPage;
+      let end = this.currentPage * this.perPage;
+      return this.getWeatherData.slice(start, end);
+    },
+    getPaginateCount() {
+      return Math.ceil(this.getWeatherData.length / this.perPage);
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
 .weather-widjet {
   padding: 0 25px 40px;
+
+  .pagination{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 70px;
+
+    li {
+      list-style: none;
+      margin-right: 10px;
+
+      a {
+        padding: 10px;
+        border: 1px solid #dad5d5;
+        border-radius: 5px;
+      }
+
+      &.active {
+        a {
+          background-color: #0072f5;
+          color: white;
+        }
+      }
+    }
+  }
 }
 
 </style>
